f07bd9bf575a1742d6503c2543919b5b1d1ccfa4
Mariya-test <mariyakrivosheeva@gmail.com>
2022-10-15 18:34:48 +0300

    Upload

--- src/test/java/chrome_w3layoutstest/ModalWindowTest.java
+++ src/test/java/chrome_w3layoutstest/ModalWindowTest.java
@@ -1,24 +1,18 @@
 package chrome_w3layoutstest;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import common_actions.CommonActions;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import static java.time.Duration.ofSeconds;
+
 
 public class ModalWindowTest extends CommonActions {
     private final String URL = "https://demo.w3layouts.com/demos_new/template_demo/03-10-2020/photoflash-liberty-demo_Free/685659620/web/index.html?_ga=2.181802926.889871791.1632394818-2083132868.1632394818";
     JavascriptExecutor js = (JavascriptExecutor)driver;
-  //  private WebDriverWait wait;
     private WebElement picture;
-    Actions action = new Actions(driver);
-  //  private WebElement modalWindow;
+
 
     @Test
     public void modalWindowTest() {
